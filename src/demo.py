2be82ebc5340cb19d091b59d5c741b77052ee116
Chen Huang <huangchen.buaa@gmail.com>
2019-11-17 03:17:01 -0800

    implement inference function to run any sentence

--- src/demo.py
+++ src/demo.py
@@ -5,3 +5,67 @@
 #   Boyu (Bill) Zhang: bzhang99@stanford.edu
 #   Xueying (Shirley) Xie: xueyingx@stanford.edu
 #
+# predict emoji given sentence
+
+# -*- coding: UTF-8 -*-
+from __future__ import unicode_literals
+import numpy as np
+import pickle
+import argparse
+import util
+import pandas as pd
+import emoji
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='DeepMoji Sentence Classification')
+    parser.add_argument('--sentence',
+                        '-s',
+                        type=str,
+                        required=True,
+                        help='sentence to analysis')
+    parser.add_argument('--model_file',
+                        '-m',
+                        type=str,
+                        required=True,
+                        default='../models/nb.pkl',
+                        help='path to save trained model')
+    parser.add_argument('--dictionary_file',
+                        '-d',
+                        type=str,
+                        required=True,
+                        default='../models/word_dictionary.txt',
+                        help='path to save word dictionary')
+    parser.add_argument('--emoji_map',
+                        '-e',
+                        type=str,
+                        required=True,
+                        default='../data/emoji_map_1791.csv',
+                        help='path to emoji mapping file')
+
+    args = parser.parse_args()
+    return args
+
+
+def main(args):
+    # step 1: load model
+    loaded_model = pickle.load(open(args.model_file, 'rb'))
+
+    # step 2: load word dictionary
+    word_dictionary = util.read_json(args.dictionary_file)
+
+    # step 3: pass sentence into matrix
+    word_vec = util.transform_text([args.sentence], word_dictionary)
+
+    # step 4: predict
+    predict = loaded_model.predict(word_vec)
+
+    # display results
+    emoji = pd.read_csv(args.emoji_map, names=['ucode'], encoding='utf-8')
+    print(emoji[predict[0] - 1])
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
--- src/main.py
+++ src/main.py
@@ -9,9 +9,11 @@
 # build-in library
 import numpy as np
 import argparse
-from torch.utils.data import DataLoader
+import pickle
+import os
 
 # customized library
+from util import write_json
 from TextDataset import TextDataset
 
 
@@ -35,12 +37,19 @@ def parse_args():
                         default='nb',
                         choices=['nb', 'svm', 'dnn'],
                         help='classifier type [nb, svm, dnn]')
+    parser.add_argument('--save_path',
+                        '-s',
+                        type=str,
+                        default='../models',
+                        help='path to save trained model and word dictionary')
     return parser.parse_args()
 
 
 def main(args):
     # step 1: load dataset
     dataset = TextDataset(args.dataset)
+    dictionary_file = os.path.join(args.save_path, 'word_dictionary.json')
+    write_json(dictionary_file, dataset.word_dictionary)
 
     # step 2: train / test split
     messages_train, messages_test = np.split(dataset.text_matrix,
@@ -52,20 +61,23 @@ def main(args):
     # step 2: load classifier
     if args.classifier == 'nb':
         from sklearn.naive_bayes import MultinomialNB
+        model_file = os.path.join(args.save_path, 'nb.pkl')
         clf = MultinomialNB()
     elif args.classifier == 'svm':
         from sklearn.svm import SVC
+        model_file = os.path.join(args.save_path, 'svm.pkl')
         clf = SVC(gamma='auto', max_iter=10000)
     else:
         NotImplementedError("DNN classifier is not implemented yet")
 
     # step 3: train classifier
     clf.fit(messages_train, labels_train)
+    pickle.dump(clf, open(model_file, 'wb'))
 
-    # step 3: evaluate classifier
-    prediction = clf.predict(messages_test)
-    accuracy = np.mean(prediction == labels_test)
-    print('%s classifier accuracy is %.3f%%' % (args.classifier, accuracy * 100))
+    # step 4: evaluate classifier
+    accuracy = clf.score(messages_test, labels_test)
+    print('%s classifier accuracy is %.3f%%' %
+          (args.classifier, accuracy * 100))
 
 
 if __name__ == '__main__':
