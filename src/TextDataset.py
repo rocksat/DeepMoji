e9cf3aeb767ab7cf76e2f85633a8283734f2ff89
Chen Huang <huangchen.buaa@gmail.com>
2019-12-08 15:03:01 -0800

    filter out emojis that appear less than 100 times

--- src/TextDataset.py
+++ src/TextDataset.py
@@ -13,9 +13,19 @@ class TextDataset(Dataset):
             transform (callable, optional): Optional transform to be applied on a sample.
         """
         self.messages, self.labels = util.load_text_dataset(txt_file)
+        self.filter()
         self.word_dictionary = util.create_dictionary(self.messages)
         self.text_matrix = util.transform_text(self.messages,
                                                self.word_dictionary)
+
+    def filter(self, occurrence=100):
+        sample_count = len(self.messages)
+        labels_count = np.bincount(self.labels)
+        select_masks = labels_count[self.labels] > occurrence
+        self.messages = [
+            self.messages[i] for i in range(sample_count) if select_masks[i]
+        ]
+        self.labels = self.labels[select_masks]
 
     def __len__(self):
         return len(self.messages)
